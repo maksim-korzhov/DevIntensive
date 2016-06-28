4416f292afbf32b66d494a6258b0652902ee5f83
korzhov.m.s <korzhov.m.s@yandex.ru>
2016-06-28 13:47:01 +0300

    Сделано приложение. Добавлен обработчик системной кнопки Назад.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ui/activities/MainActivity.java
@@ -1,28 +1,50 @@
 package com.softdesign.devintensive.ui.activities.ui.activities;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.ui.activities.data.managers.DataManager;
 import com.softdesign.devintensive.ui.activities.utils.ConstantManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    private int mCurrectEditorMode = 0;
+
+    private DataManager mDataManager;
     private ImageView mCallImg;
     private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit, mUserBio;
+
+    private List<EditText> mUserInfoViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +52,52 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
 
-        mCallImg = (ImageView) findViewById(R.id.call_img);
-        mCallImg.setOnClickListener(this);
+        mDataManager = DataManager.getInstance();
 
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+
+        mUserPhone = (EditText) findViewById(R.id.phone_et);
+        mUserMail = (EditText) findViewById(R.id.email_et);
+        mUserVk = (EditText) findViewById(R.id.vk_et);
+        mUserGit = (EditText) findViewById(R.id.github_et);
+        mUserBio = (EditText) findViewById(R.id.about_et);
+
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserMail);
+        mUserInfoViews.add(mUserVk);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUserBio);
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mFab.setOnClickListener(this);
+
+        setupToolbar();
+        setupDrawer();
+        loadUserInfoValue();
+
+        List<String> test = mDataManager.getPreferencesManager().loadUserProfileData();
 
         if( savedInstanceState == null ) {
             // активити запущено впервые
-            showSnackbar("Activity запускается впервые");
+            //showSnackbar("Activity запускается впервые");
         } else {
-            // активити уже запускалось
-            showSnackbar("Activity уже запускалось");
+            mCurrectEditorMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrectEditorMode);
         }
+    }
+
+    // Обработка клика по эдементу меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if( item.getItemId() == android.R.id.home ) {
+            // Указываем, с какой стороны он должен открываться
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -63,6 +119,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onPause();
 
         Log.d(TAG, "onPause");
+        //saveUserInfoValue();
     }
 
     @Override
@@ -89,16 +146,30 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch( v.getId() ) {
-            case R.id.call_img:
-                /*showProgress();
-                runWithDelay();*/
+            case R.id.fab:
+                if( mCurrectEditorMode == 0 ) {
+                    changeEditMode(1);
+                    mCurrectEditorMode = 1;
+                } else {
+                    changeEditMode(0);
+                    mCurrectEditorMode = 0;
+                }
                 break;
+        }
+    }
+
+    // Обработка нажатия кнопки назад
+    @Override
+    public void onBackPressed() {
+        if( mNavigationDrawer.isDrawerOpen(GravityCompat.START) ) {
+            mNavigationDrawer.closeDrawer(GravityCompat.START);
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrectEditorMode);
     }
 
 /*    private void runWithDelay() {
@@ -114,5 +185,71 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private void showSnackbar( String message ) {
         Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar() {
+        // Заменим actionbar на toolbar, он более гибкий и позволяет настраивать себя
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if( actionBar != null ) {
+            // Установим картинку главной кнопки
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            // отображать кнопку в actionBar
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    showSnackbar(item.getTitle().toString());
+                    item.setChecked(true);
+                    mNavigationDrawer.closeDrawer(GravityCompat.START);
+                    return false;
+                }
+            });
+        }
+    }
+
+    /**
+     * переключает режим редактирования
+     * @param mode если 1 - режим редактирования, 0 - режим просмотра
+     */
+    private void changeEditMode( int mode ) {
+        if( mode == 1 ) {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        } else {
+            mFab.setImageResource(R.drawable.ic_create_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+                //saveUserInfoValue();
+            }
+        }
+    }
+
+    private void loadUserInfoValue() {
+        List<String> userData = mDataManager.getPreferencesManager().loadUserProfileData();
+        for (int i = 0; i < userData.size(); i++) {
+            mUserInfoViews.get(i).setText(userData.get(i));
+        }
+    }
+
+    private void saveUserInfoValue() {
+        List<String> userData = new ArrayList<>();
+        for (EditText userFieldView : mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferencesManager().saveUserProfileData(userData);
     }
 }
