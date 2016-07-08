77957452ed7cd3faca2331be7abd9816d30ca1d6
korzhov.m.s <korzhov.m.s@yandex.ru>
2016-07-08 21:04:27 +0300

    Реализован вызов неявных интентов для звонков, почты, браузера.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ui/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.softdesign.devintensive.ui.activities.ui.activities;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -15,6 +19,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -22,6 +27,8 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -78,6 +85,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private Uri mSelectedimage = null;
     private ImageView mProfileImage;
 
+    private ImageView mCallTrigger;
+    private ImageView mEmailTrigger;
+    private ImageView mVkTrigger;
+    private ImageView mGithubTrigger;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -111,11 +123,23 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mFab.setOnClickListener(this);
         mProfilePlaceholder.setOnClickListener(this);
 
+        mCallTrigger = (ImageView) findViewById(R.id.call_trigger);
+        mEmailTrigger = (ImageView) findViewById(R.id.email_trigger);
+        mVkTrigger = (ImageView) findViewById(R.id.vk_trigger);
+        mGithubTrigger = (ImageView) findViewById(R.id.github_trigger);
+
+        /* Setup intent triggers */
+        mCallTrigger.setOnClickListener(this);
+        mEmailTrigger.setOnClickListener(this);
+        mVkTrigger.setOnClickListener(this);
+        mGithubTrigger.setOnClickListener(this);
+
         setupToolbar();
         setupDrawer();
         loadUserInfoValue();
         Picasso.with(this)
                 .load(mDataManager.getPreferencesManager().loadUserPhoto())
+                .placeholder(R.drawable.user_photo) // TODO: 07.07.2016 Сделать placeholder + transform + crop
                 .into(mProfileImage);
 
         List<String> test = mDataManager.getPreferencesManager().loadUserProfileData();
@@ -207,6 +231,62 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 // TODO 6.07.2016 Следать выбор из галлереи или из камеры загружаем фото
                 showDialog(ConstantManager.LOAD_PROFILE_PHOTO);
                 break;
+
+            case R.id.call_trigger:
+                startCallIntent();
+                break;
+
+            case R.id.email_trigger:
+                startEmailIntent();
+                break;
+
+            case R.id.github_trigger:
+            case R.id.vk_trigger:
+                startBrowserIntent();
+                break;
+        }
+    }
+
+    private void startEmailIntent() {
+        String email = mUserMail.getText().toString();
+
+        if (email.equals("null")) {
+            showSnackbar(getString(R.string.error_user_profile_email_empty));
+        } else {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_EMAIL, email);
+            startActivity(intent);
+        }
+    }
+
+
+    private void startCallIntent() {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+
+            String callPhone = mUserPhone.getText().toString();
+
+            if (callPhone.equals("null")) {
+                showSnackbar(getString(R.string.error_user_profile_phone_empty));
+            } else {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + callPhone));
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void startBrowserIntent() {
+        String stringUri = mUserGit.getText().toString();
+
+        if( stringUri.equals("null") ) {
+            showSnackbar(getString(R.string.error_user_profile_link_empty));
+        } else {
+            Uri uri = Uri.parse(stringUri);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
         }
     }
 
@@ -363,19 +443,48 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     private void loadPhotoFromCamera() {
-        Intent takeCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Intent takeCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        try {
-            mPhotoFile = createImageFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-            // TODO: 06.07.2016 обработать ошибку
+            try {
+                mPhotoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // TODO: 06.07.2016 обработать ошибку
+            }
+
+            if (mPhotoFile != null) {
+                // TODO: 06.07.2016 передать фотофайл в интент
+                takeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
+                startActivityForResult(takeCaptureIntent, ConstantManager.REQUEST_CAMERA_PICTURE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, ConstantManager.CAMERA_REQUEST_PERMISSION_CODE);
+
+            Snackbar.make(mCoordinatorLayout, "Для корректной работы приложения необходимо дать требуемые разрешения", Snackbar.LENGTH_LONG)
+                .setAction("Разрешить", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openApplicationSettings();
+                    }
+                }).show();
         }
+    }
 
-        if( mPhotoFile != null ) {
-            // TODO: 06.07.2016 передать фотофайл в интент
-            takeCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPhotoFile));
-            startActivityForResult(takeCaptureIntent, ConstantManager.REQUEST_CAMERA_PICTURE);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if( requestCode == ConstantManager.CAMERA_REQUEST_PERMISSION_CODE && grantResults.length == 2 ) {
+            if( grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+                // TODO 7.07.2016
+            }
+
+            if( grantResults[1] == PackageManager.PERMISSION_GRANTED ) {
+                // TODO 7.07.2016
+            }
         }
     }
 
@@ -413,17 +522,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                             case 0:
                                 // TODO 6.07.2016 Выбрать из галереи
                                 loadPhotoFromGallery();
-                                showSnackbar("Выбрать из галереи");
+                                //showSnackbar("Выбрать из галереи");
                                 break;
                             case 1:
                                 // TODO 6.07.2016 Сделать снимок
                                 loadPhotoFromCamera();
-                                showSnackbar("Сделать снимок");
+                                //showSnackbar("Сделать снимок");
                                 break;
                             case 2:
                                 // TODO 6.07.2016 Отменить
                                 dialog.cancel();
-                                showSnackbar("Отменить");
+                                //showSnackbar("Отменить");
                                 break;
                         }
                     }
@@ -442,6 +551,13 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, image.getAbsolutePath());
+
+        this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
         return image;
     }
 
@@ -457,4 +573,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mDataManager.getPreferencesManager().saveUserPhoto(mSelectedimage);
     }
 
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+
+        startActivityForResult(appSettingsIntent, ConstantManager.PERMISSION_REQUEST_SETTINGS_CODE);
+    }
 }
